05ad9b814281dd48f1d5afb90b0143742bc2dd43
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-02-11 20:41:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Root from './Hooks-Context';
+import ValueContext from './ValueContext';
+import HooksEffects from './Hooks-Effect';
+import { useState } from 'react';
 function App() {
+  const [hide,setHide]=useState(false);
   return (
-    // <div>
-    //     <h1>hello world..</h1>
-    //  </div>
-    <div class="head">
-      <h2>Hello Man</h2>
-    </div>
+    <>
+      <div>
+        <h1>hello world..</h1>
+      </div>
+      <input type="checkbox"  onChange={()=> setHide(!hide)} />
+        Click to Hide
+      {hide ? null:<HooksEffects/>}
+      <ValueContext.Provider value={hide}>
+        <Root />
+      </ValueContext.Provider>
+    </>
   );
-}
-
-export default App;
+}export default App;
