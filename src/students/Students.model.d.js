700f9ecc85d8ddef7ac75271a7c64c10db8be3cd
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-02-23 13:23:50 +0530

    Add files via upload

---
+++ src/students/Students.model.d.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} StudentDTO
+ * @property {number} id - The ID of the student.
+ * @property {string} rollno - The rollno of the student.
+ * @property {string} name - The name of the student.
+ * @property {string} photo - The photo URL or base64 representation.
+ */
+
+// Exporting the type for use in other files
+export default StudentDTO;
