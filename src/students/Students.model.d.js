e275b4331493c1605d807ce200bc358c84039380
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-02-23 19:04:25 +0530

    Update Students.model.d.js

--- src/students/Students.model.d.js
+++ src/students/Students.model.d.js
@@ -1,10 +1,16 @@
+/* global mainpageDTO */
+
 /**
  * @typedef {Object} StudentDTO
  * @property {number} id - The ID of the student.
- * @property {string} rollno - The rollno of the student.
+ * @property {string} rollNo - The roll number of the student.
  * @property {string} name - The name of the student.
  * @property {string} photo - The photo URL or base64 representation.
  */
 
-// Exporting the type for use in other files
-export default StudentDTO;
+/**
+ * @typedef {object} mainpageDTO
+ * @property {StudentDTO[]} liststudents - The list of students.
+ * @property {StudentDTO[]} listtoppers - The topperlist of students.
+ */
+export default mainpageDTO;
