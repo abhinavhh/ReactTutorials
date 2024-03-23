bd7b599dd2ac4fad09e38164b656181ae1f528bb
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-23 14:04:56 +0530

    Update CreateCategory.jsx

--- src/categories/CreateCategory.jsx
+++ src/categories/CreateCategory.jsx
@@ -1,1 +1,12 @@
 
+export default function CreateCateg() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Create Category</h3>
+                </div>
+            </div>
+        </>
+    );
+}
