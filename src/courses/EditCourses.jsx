f316c64f8c7237459b2badc61369b8510a943b65
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-23 14:06:51 +0530

    Add files via upload

---
+++ src/courses/EditCourses.jsx
@@ -0,0 +1,11 @@
+export default function EditCourse() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Edit Course</h3>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/courses/IndexCourses.jsx
@@ -0,0 +1,13 @@
+function IndexCourse() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>All Courses</h3>
+                    <button type="button" class="btn btn-primary">Select Course</button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default IndexCourse;
