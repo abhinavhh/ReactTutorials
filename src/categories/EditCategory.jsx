7379392f085367679c14061ea013739332a17329
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-23 14:05:27 +0530

    Add files via upload

---
+++ src/categories/EditCategory.jsx
@@ -0,0 +1,11 @@
+export default function EditCateg() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Edit Category</h3>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/categories/IndexCategories.jsx
@@ -0,0 +1,11 @@
+export default function IndexCateg() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Index Category</h3>
+                </div>
+            </div>
+        </>
+    );
+}
