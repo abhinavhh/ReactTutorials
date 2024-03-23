5b7d97e84a38253ee053e4465492f877d9520d12
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-23 14:09:00 +0530

    Add files via upload

---
+++ src/instructors/EditInstructor.jsx
@@ -0,0 +1,11 @@
+export default function EditInstr() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Edit Instructor</h3>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/instructors/IndexInstructors.jsx
@@ -0,0 +1,11 @@
+export default function IndexInstr() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Index Instructor</h3>
+                </div>
+            </div>
+        </>
+    );
+}
