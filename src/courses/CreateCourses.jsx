d5bfd265ef6126be7985d391cdec9576eaeacb54
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-23 14:06:23 +0530

    Update CreateCourses.jsx

--- src/courses/CreateCourses.jsx
+++ src/courses/CreateCourses.jsx
@@ -1,1 +1,13 @@
 
+
+export default function CreateCourse() {
+    return(
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h3>Create Course</h3>
+                </div>
+            </div>
+        </>
+    );
+}
