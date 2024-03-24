6facfdb21793068d052bf4733c604f9fc11ed81b
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-24 18:27:45 +0530

    Update SearchStudent.jsx

--- src/students/SearchStudent.jsx
+++ src/students/SearchStudent.jsx
@@ -1,11 +1,13 @@
-export default function SearchStud() {
+function SearchStud() {
     return(
     <>
         <div className="card">
             <div className="card-body">
-                <h3>Search Student</h3>
+                <h3>All Students</h3>
+                <button type="button" class="btn btn-primary">Select Student</button>
             </div>
         </div>
     </>
-    );
+    )
 }
+export default SearchStud;
