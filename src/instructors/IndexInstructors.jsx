f13e1443ace61c735dd19af516a55e29d8e6ea87
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-24 18:22:56 +0530

    Update IndexInstructors.jsx

--- src/instructors/IndexInstructors.jsx
+++ src/instructors/IndexInstructors.jsx
@@ -4,6 +4,7 @@ export default function IndexInstr() {
             <div className="card">
                 <div className="card-body">
                     <h3>Index Instructor</h3>
+                    <button type="button" class="btn btn-primary">Select Instructor</button>
                 </div>
             </div>
         </>
