48adef2e8830bddb94a98d225932c80c2ad6ab04
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-24 18:19:53 +0530

    Update EditCourses.jsx

--- src/courses/EditCourses.jsx
+++ src/courses/EditCourses.jsx
@@ -1,11 +1,19 @@
-export default function EditCourse() {
-    return(
-        <>
-            <div className="card">
-                <div className="card-body">
-                    <h3>Edit Course</h3>
-                </div>
-            </div>
-        </>
-    );
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function EditCourse() {
+  const { id } = useParams();
+
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h3>Edit Course</h3>
+          <p>Select Course id is {id}</p>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default EditCourse;
