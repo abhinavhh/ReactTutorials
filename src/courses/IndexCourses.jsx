00de6aae93c7e30146478902d926af56a7d6777c
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-24 18:20:17 +0530

    Update IndexCourses.jsx

--- src/courses/IndexCourses.jsx
+++ src/courses/IndexCourses.jsx
@@ -1,13 +1,20 @@
+import { NavLink } from "react-router-dom";
 function IndexCourse() {
     return(
         <>
             <div className="card">
                 <div className="card-body">
                     <h3>All Courses</h3>
-                    <button type="button" class="btn btn-primary">Select Course</button>
+                    <NavLink ClassName="active" to="./CreateCourses">
+                        <button type="button" class="btn btn-primary">Create Course</button>
+                    </NavLink>
+                    <br />
+                    <NavLink ClassName="active" to="./EditCourses">
+                        <button type="button" class="btn btn-primary">Edit Course</button>
+                    </NavLink>
                 </div>
             </div>
         </>
-    );
+    )
 }
 export default IndexCourse;
