642d97946cecc802a403937ff41be8451cc522c1
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-24 18:23:40 +0530

    Update IndexCategories.jsx

--- src/categories/IndexCategories.jsx
+++ src/categories/IndexCategories.jsx
@@ -4,6 +4,7 @@ export default function IndexCateg() {
             <div className="card">
                 <div className="card-body">
                     <h3>Index Category</h3>
+                    <button type="button" class="btn btn-primary">Select Category</button>
                 </div>
             </div>
         </>
