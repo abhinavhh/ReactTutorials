5607ca1ed626ab005fee0f0c76065fe7a1e0176d
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-27 15:44:36 +0530

    Update EditCourses.jsx

--- src/courses/EditCourses.jsx
+++ src/courses/EditCourses.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 function EditCourse() {
   const { id } = useParams();
@@ -9,7 +10,15 @@ function EditCourse() {
       <div className="card">
         <div className="card-body">
           <h3>Edit Course</h3>
-          <p>Select Course id is {id}</p>
+          {/* <p>Select Course id is {id}</p> */}
+          <CourseForm
+            model={{name:"Andrroid Mobile Application"}}
+            onSubmit={async (values)=>{
+              await new Promise((r)=>setTimeout(r,500));
+              alert(JSON.stringify(id,null,2));
+              alert(JSON.stringify(values,null,2));
+            }}
+            />
         </div>
       </div>
     </>
