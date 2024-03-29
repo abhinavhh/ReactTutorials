475d0d0b307c12383db1e8c12d1d4cde14c479c9
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-29 17:19:43 +0530

    Update EditCourses.jsx

--- src/courses/EditCourses.jsx
+++ src/courses/EditCourses.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import CourseForm from "./CourseForm";
+import { courseCreationDTOWithIdAndName } from "./courses.model.d";
 
 function EditCourse() {
-  const { id } = useParams();
+  const { id } = useParams(1);
+
+  // Assuming the CourseForm component accepts a 'model' prop
+  const initialModel = { name: "Android Mobile Application" }; // Define your initial model here
 
   return (
     <>
@@ -12,13 +16,13 @@ function EditCourse() {
           <h3>Edit Course</h3>
           {/* <p>Select Course id is {id}</p> */}
           <CourseForm
-            model={{name:"Andrroid Mobile Application"}}
-            onSubmit={async (values)=>{
-              await new Promise((r)=>setTimeout(r,500));
-              alert(JSON.stringify(id,null,2));
-              alert(JSON.stringify(values,null,2));
+            model={{ id: id, name: "Initial Course Name"  }}
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 500));
+              alert(JSON.stringify(id, null, 2));
+              alert(JSON.stringify(values, null, 2));
             }}
-            />
+          />
         </div>
       </div>
     </>
