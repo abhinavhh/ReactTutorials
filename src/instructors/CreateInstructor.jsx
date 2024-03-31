990fa372bc7f85bb0995059db8de621e33c04a38
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-03-31 16:28:49 +0530

    Update CreateInstructor.jsx

--- src/instructors/CreateInstructor.jsx
+++ src/instructors/CreateInstructor.jsx
@@ -1,11 +1,20 @@
+import InstructorForm from "./InstructorForm";
+
 export default function CreateInstr() {
     return(
         <>
             <div className="card">
                 <div className="card-body">
                     <h3>Create Instructor</h3>
+                    <InstructorForm
+                        model={{ name:"", dateOfBirth:undefined}}
+                        onSubmit={async (values) => {
+                            await new Promise((r) => setTimeout(r,1000));
+                            alert(JSON.stringify(values,null,2));
+                        }}
+                    />
                 </div>
             </div>
         </>
-    );
+    )
 }
