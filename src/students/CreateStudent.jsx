531cfba150897ad0c48f8907f95513518c599ca5
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-04-12 09:53:30 +0530

    Update CreateStudent.jsx

--- src/students/CreateStudent.jsx
+++ src/students/CreateStudent.jsx
@@ -1,11 +1,25 @@
-export default function CreateStud() {
+import StudentForm from "./StudentForm";
+
+function CreateStud() {
     return(
         <>
             <div className="card">
             <div className="card-body">
                 <h3>Create Student</h3>
+                <StudentForm
+                    model={{
+                        name:"",
+                        rollno:"",
+                        isActive:false,
+                        dateofBirth:undefined,
+                    }}
+                    onSubmit={async (values)=>{
+                        await new Promise((r)=>setTimeout(r,1000));
+                        alert(JSON.stringify(values,null,2));
+                    }}
+                />
             </div>
         </div>
         </>
-    );
-}
+    )
+}export default CreateStud;
