9e20224c9dfefa4874c7dead264d046fd4525e0b
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-04-12 09:46:41 +0530

    Update EditCategory.jsx

--- src/categories/EditCategory.jsx
+++ src/categories/EditCategory.jsx
@@ -1,11 +1,23 @@
+import CategoryForm from "./CategoryForm";
+import { useParams } from "react-router-dom";
 export default function EditCateg() {
+    const { id } = useParams(9);
+
     return(
         <>
             <div className="card">
                 <div className="card-body">
                     <h3>Edit Category</h3>
+                    <CategoryForm
+                        model={{ id: id, name: "Initial Course Name"  }}
+                        onSubmit={async (values) => {
+                        await new Promise((r) => setTimeout(r, 500));
+                        alert(JSON.stringify(id, null, 2));
+                        alert(JSON.stringify(values, null, 2));
+                        }}
+                    />
                 </div>
             </div>
         </>
-    );
+    )
 }
