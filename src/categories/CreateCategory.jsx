2314493f8cd14ec3a34fba4eaf0adc1191c51241
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2024-04-12 09:46:18 +0530

    Update CreateCategory.jsx

--- src/categories/CreateCategory.jsx
+++ src/categories/CreateCategory.jsx
@@ -1,3 +1,4 @@
+import CategoryForm from "./CategoryForm";
 
 export default function CreateCateg() {
     return(
@@ -5,8 +6,14 @@ export default function CreateCateg() {
             <div className="card">
                 <div className="card-body">
                     <h3>Create Category</h3>
+                    <CategoryForm
+                        model={{name: ""}}
+                        onSubmit={async (values)=>{
+                            await new Promise((r)=>setTimeout(r,1000));
+                            alert(JSON.stringify(values,null,2));
+                        }}/>
                 </div>
             </div>
         </>
-    );
+    )
 }
