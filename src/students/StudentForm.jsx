3c3ce07ea88736bce5ddd3c77d96c11e7cc4c313
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-04-19 00:14:24 +0530

    Update StudentForm.jsx

--- src/students/StudentForm.jsx
+++ src/students/StudentForm.jsx
@@ -1,51 +1,92 @@
-import { Formik , Form} from "formik";
-import * as Yup from 'yup';
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import PropTypes from "prop-types";
 import ButtonSubmit from "../assets/ButtonSubmit";
-import PropTypes from 'prop-types';
 import TextField from "../forms/TextField";
 import ImageField from "../forms/ImageField";
 import DateField from "../forms/DateField";
 import CheckboxField from "../forms/CheckboxField";
 import { Link } from "react-router-dom";
-export default function StudentForm(props){
-    return(
-        <Formik
-            initialValues={props.model}
-            onSubmit={props.onSubmit}
-            validationSchema={Yup.object({
-                name:Yup.string()
-                    .required('Category Name should not be blank')
-                    .matches(/^[A-Z].*$/, 'Category Name should start with an uppercase letter'),
-                rollno:Yup.string()
-                    .required("Roll Number should not be blank"),
-                dateofBirth:Yup.date().nullable().required("Date should not be blank"),
-            })}
-        >
-            {(formikProps) => (
-                <Form>
-                <TextField field="name" displayName="Student Name" />
-                <TextField field="rollno" displayName="Roll Number" />
-                <DateField field="dateOfBirth" displayName="Date of Birth" />
-                <CheckboxField field="isActive" displayName="Active"/>
-                <ImageField
-                    displayName="Photograph"
-                    field="image"
-                    imageURL={props.model.photoURL}
-                />
-                <ButtonSubmit disabled={formikProps.isSubmitting} type="submit" className="btn btn-primary">
-                    Submit
-                </ButtonSubmit>
-                <Link className="btn btn-secondary ms-3" to="/student/SearchStudent">
-                    Cancel
-                </Link>
-                </Form>
-            )}
-        </Formik>
-    )
+import MultipleSelector from "../forms/MultipleSelector";
+
+export default function StudentForm(props) {
+  const [selectedCourses, setSelectedCourses] = useState(
+    mapToModel(props.selectedCourses)
+  );
+
+  const [unSelectedCourses, setUnSelectedCourses] = useState(
+    mapToModel(props.unSelectedCourses)
+  );
+
+  const [selectedCategories, setSelectedCategories] = useState(
+    mapToModel(props.selectedCategories)
+  );
+
+  const [unSelectedCategories, setUnSelectedCategories] = useState(
+    mapToModel(props.unSelectedCategories)
+  );
+
+  function mapToModel(items) {
+    return items.map((item) => {
+      return { key: item.id, value: item.name };
+    });
+  }
+
+  return (
+    <Formik
+      initialValues={props.model}
+      onSubmit={props.onSubmit}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .required("Category Name should not be blank")
+          .matches(/^[A-Z].*$/, "Category Name should start with an uppercase letter"),
+        rollno: Yup.string().required("Roll Number should not be blank"),
+        dateOfBirth: Yup.date().nullable().required("Date should not be blank"),
+      })}
+    >
+      {(formikProps) => (
+        <Form>
+          <TextField field="name" displayName="Student Name" />
+          <TextField field="rollno" displayName="Roll Number" />
+          <DateField field="dateOfBirth" displayName="Date of Birth" />
+          <CheckboxField field="isActive" displayName="Active" />
+          <ImageField
+            displayName="Photograph"
+            field="image"
+            imageURL={props.model.photoURL}
+          />
+          <MultipleSelector
+            displayName="Categories"
+            unSelected={unSelectedCategories}
+            selected={selectedCategories}
+            onChange={(selected, unSelected) => {
+              setSelectedCategories(selected);
+              setUnSelectedCategories(unSelected);
+            }}
+          />
+          <MultipleSelector
+            displayName="Courses"
+            unSelected={unSelectedCourses}
+            selected={selectedCourses}
+            onChange={(selected, unSelected) => {
+              setSelectedCourses(selected);
+              setUnSelectedCourses(unSelected);
+            }}
+          />
+          <ButtonSubmit
+            disabled={formikProps.isSubmitting}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Submit
+          </ButtonSubmit>
+          <Link className="btn btn-secondary ms-3" to="/student/SearchStudent">
+            Cancel
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
 }
-StudentForm.propTypes={
-    model: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-      }).isRequired,
-      onSubmit: PropTypes.func.isRequired,
-}
+
